9f7ac4d2c71b08058a0ed539d2fcd19317011839
Kai Shiao <kshiao13@gmail.com>
2016-02-29 14:20:39 -0800

    Finalized continuous animation.

--- css3FadeEffects/css3FadeEffects.js
+++ css3FadeEffects/css3FadeEffects.js
@@ -5,13 +5,8 @@ function performAnimation(){
 	function fadeFromView(){
 		$('button').eq(0).removeClass('fadeIntoView').addClass('fadeFromView');
 	}
-	setTimeout(fadeFromView,3000);
+	fadeIntoView();
+	setTimeout(fadeFromView,1000);
 }
 
-function stopAnimation(){
-	$currentClass=$('button').eq(0).attr('class');
-	$('button').eq(0).removeClass($currentClass).addClass('display');
-}
-
-$('button').eq(0).on('click',performAnimation);
-$('button').eq(0).on('dblclick',stopAnimation);
+$('document').ready(performAnimation);
